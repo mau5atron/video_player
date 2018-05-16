251b519c39460c3395c8c10708bfbd4944c89260
mau5atron <gabrielbetancourt96@outlook.com>
2018-05-16 15:43:00 -0700

    added query selectors for player

---
+++ public/scripts.js
@@ -0,0 +1,10 @@
+ // 1. Get the elements in our player
+ const player = document.querySelector('.player');
+ const video = player.querySelector('.viewer');
+ const progress = player.querySelector('.progress');
+ const progressBar = player.querySelector('.progress__filled');
+ const toggle = player.querySelector('.toggle');
+ const skipButtons = player.querySelectorAll('[data-skip]');
+ const ranges = player.querySelectorAll('.player__slider');
+ // 2. Build out our functions
+
